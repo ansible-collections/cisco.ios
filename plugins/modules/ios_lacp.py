4846f2c57769bf8ba6e231453b89ac795122b36a
Sumit Jaiswal <sjaiswal@redhat.com>
2020-05-12 16:25:24 +0530

    fix module spacing

--- plugins/modules/ios_lacp.py
+++ plugins/modules/ios_lacp.py
@@ -24,7 +24,8 @@ __metaclass__ = type
 
 ANSIBLE_METADATA = {"metadata_version": "1.1", "supported_by": "Ansible"}
 
-DOCUMENTATION = """module: ios_lacp
+DOCUMENTATION = """
+module: ios_lacp
 short_description: LACP resource module
   IOS devices.
 description: This module provides declarative management of Global LACP on Cisco IOS
