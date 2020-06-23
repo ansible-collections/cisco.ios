7a4babfd985d02b2bf630d26107f41b87b353ba1
Nathaniel Case <Qalthos@gmail.com>
2020-06-23 17:46:00 -0400

    Clean up boilerplate ignores

--- plugins/module_utils/network/ios/providers/cli/config/base.py
+++ plugins/module_utils/network/ios/providers/cli/config/base.py
@@ -2,6 +2,9 @@
 # (c) 2019, Ansible by Red Hat, inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
 from ansible.module_utils.six import iteritems
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
     to_list,
--- tests/unit/mock/path.py
+++ tests/unit/mock/path.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
 from ansible_collections.cisco.ios.tests.unit.compat.mock import MagicMock
 from ansible.utils.path import unfrackpath
 
