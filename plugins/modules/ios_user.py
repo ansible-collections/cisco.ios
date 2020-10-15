177ce4e3843d7ca228237f2a1fad5946320f45da
lduck <2730100+lduck@users.noreply.github.com>
2020-10-15 12:58:00 +0200

    Update ios_user.py

--- plugins/modules/ios_user.py
+++ plugins/modules/ios_user.py
@@ -399,7 +399,7 @@ def map_obj_to_commands(updates, module):
             add_ssh(commands, want, want["sshkey"])
         if needs_update(want, have, "configured_password"):
             if update_password == "always" or not have:
-                if have and password_type != have["password_type"]:
+                if have and have['password_type'] and password_type != have["password_type"]:
                     module.fail_json(
                         msg="Can not have both a user password and a user secret."
                         + " Please choose one or the other."
