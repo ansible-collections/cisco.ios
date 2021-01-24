3991def1e1aac2dd435f986fcad1a996ad0b41cb
Sumit Jaiswal <sjaiswal@redhat.com>
2021-01-25 02:18:33 +0530

    remove utils change

--- plugins/module_utils/network/ios/utils/utils.py
+++ plugins/module_utils/network/ios/utils/utils.py
@@ -11,48 +11,6 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import socket
-
-try:
-    """Python 3.3+ branch."""
-    from collections.abc import (
-        MappingView,
-        ItemsView,
-        KeysView,
-        ValuesView,
-        Mapping,
-        MutableMapping,
-        Sequence,
-        MutableSequence,
-        Set,
-        MutableSet,
-        Container,
-        Hashable,
-        Sized,
-        Callable,
-        Iterable,
-        Iterator,
-    )
-except ImportError:
-    """Use old lib location under 2.6-3.2."""
-    from collections import (
-        MappingView,
-        ItemsView,
-        KeysView,
-        ValuesView,
-        Mapping,
-        MutableMapping,
-        Sequence,
-        MutableSequence,
-        Set,
-        MutableSet,
-        Container,
-        Hashable,
-        Sized,
-        Callable,
-        Iterable,
-        Iterator,
-    )
-from itertools import chain
 from ansible.module_utils.six import iteritems
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
     is_masklen,
@@ -319,77 +277,6 @@ def validate_n_expand_ipv4(module, want):
 def netmask_to_cidr(netmask):
     # convert netmask to cidr and returns the cidr notation
     return str(sum([bin(int(x)).count("1") for x in netmask.split(".")]))
-
-
-def sort_list(val):
-    if isinstance(val, list):
-        return sorted(val)
-    return val
-
-
-def dict_merge(base, other):
-    """ Return a new dict object that combines base and other
-
-    This will create a new dict object that is a combination of the key/value
-    pairs from base and other.  When both keys exist, the value will be
-    selected from other.  If the value is a list object, the two lists will
-    be combined and duplicate entries removed.
-
-    :param base: dict object to serve as base
-    :param other: dict object to combine with base
-
-    :returns: new combined dict object
-    """
-    if not isinstance(base, dict):
-        raise AssertionError("`base` must be of type <dict>")
-    if not isinstance(other, dict):
-        raise AssertionError("`other` must be of type <dict>")
-
-    combined = dict()
-
-    for key, value in iteritems(base):
-        if isinstance(value, dict):
-            if key in other:
-                item = other.get(key)
-                if item is not None:
-                    if isinstance(other[key], Mapping):
-                        combined[key] = dict_merge(value, other[key])
-                    else:
-                        combined[key] = other[key]
-                else:
-                    combined[key] = item
-            else:
-                combined[key] = value
-        elif isinstance(value, list):
-            if key in other:
-                item = other.get(key)
-                if item is not None:
-                    try:
-                        combined[key] = list(set(chain(value, item)))
-                    except TypeError:
-                        value.extend([i for i in item if i not in value])
-                        combined[key] = value
-                else:
-                    combined[key] = item
-            else:
-                combined[key] = value
-        else:
-            if key in other:
-                other_value = other.get(key)
-                if other_value is not None:
-                    if sort_list(base[key]) != sort_list(other_value):
-                        combined[key] = other_value
-                    else:
-                        combined[key] = value
-                else:
-                    combined[key] = other_value
-            else:
-                combined[key] = value
-
-    for key in set(other.keys()).difference(base.keys()):
-        combined[key] = other.get(key)
-
-    return combined
 
 
 def normalize_interface(name):
