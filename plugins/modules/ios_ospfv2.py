063e4cc0388d3aede7d3ba294fabe16ab89ef7e1
Sumit Jaiswal <sjaiswal@redhat.com>
2021-01-28 01:23:43 +0530

    update doc

--- plugins/modules/ios_ospfv2.py
+++ plugins/modules/ios_ospfv2.py
@@ -900,13 +900,13 @@ options:
                 type: int
   running_config:
     description:
-    - The module, by default, will connect to the remote device and retrieve the current
-      running-config to use as a base for comparing against the contents of source.
-      There are times when it is not desirable to have the task get the current running-config
-      for every task in a playbook.  The I(running_config) argument allows the implementer
-      to pass in the configuration to use as the base config for comparison. This
-      value of this option should be the output received from device by executing
-      command.
+      - This option is used only with state I(parsed).
+      - The value of this option should be the output received from the IOS
+        device by executing the command B(sh running-config | section ^router ospf).
+      - The state I(parsed) reads the configuration from C(running_config)
+        option and transforms it into Ansible structured data as per the
+        resource module's argspec and the value is then returned in the
+        I(parsed) key within the result.
     type: str
   state:
     description:
