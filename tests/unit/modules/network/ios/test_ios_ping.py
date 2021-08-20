8ccee5e5b38b57a121d8d7bf9058029bc83ad606
KB-perByte <sagpaul@redhat.com>
2021-08-20 15:46:18 +0530

    sanity for 2 12 updated

--- tests/unit/modules/network/ios/test_ios_ping.py
+++ tests/unit/modules/network/ios/test_ios_ping.py
@@ -51,7 +51,9 @@ class TestIosPingModule(TestIosModule):
             output = list()
 
             for command in commands:
-                filename = str(command).split(" | ")[0].replace(" ", "_")
+                filename = (
+                    str(command).split(" | ", maxsplit=1)[0].replace(" ", "_")
+                )
                 output.append(load_fixture("ios_ping_%s" % filename))
             return output
 
