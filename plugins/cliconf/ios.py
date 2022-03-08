8fdb0f01701eaf87220698514211139bcc0879a2
KB-perByte <sagpaul@redhat.com>
2022-03-08 21:36:56 +0530

    update name

--- plugins/cliconf/ios.py
+++ plugins/cliconf/ios.py
@@ -22,7 +22,7 @@ __metaclass__ = type
 
 DOCUMENTATION = """
 author:
-- Ansible Networking Team
+- Ansible Networking Team (@ansible-network)
 name: ios
 short_description: Use ios cliconf to run command on Cisco IOS platform
 description:
