80e8d8939b74e6b22c7a836d30633285c7523ece
Sagar Paul <sagpaul@redhat.com>
2022-05-16 23:15:24 +0530

    update regex pattern (#577)
    [ntp] fix to handle server attributes gracefully on no match
    
    SUMMARY
    
    NTP fix to handle server attributes gracefully on no match
    
    ISSUE TYPE
    
    
    Bugfix Pull Request
    
    COMPONENT NAME
    
    ios_ntp
    ADDITIONAL INFORMATION
    
    Reviewed-by: Rohit Thakur <rohitthakur2590@outlook.com>

--- plugins/modules/ios_ntp.py
+++ plugins/modules/ios_ntp.py
@@ -127,22 +127,20 @@ from ansible_collections.cisco.ios.plugins.module_utils.network.ios.ios import (
 
 def parse_server(line, dest):
     if dest == "server":
+        vrf, server = None, None
         match = re.search(
             "(ntp\\sserver\\s)(vrf\\s\\w+\\s)?(\\d+\\.\\d+\\.\\d+\\.\\d+)",
             line,
             re.M,
         )
 
-        if match.group(2) and match.group(3):
+        if match and match.group(2) and match.group(3):
             vrf = match.group(2)
             server = match.group(3)
-
             return vrf, server
 
-        if match.group(3):
-            vrf = None
+        if match and match.group(3):
             server = match.group(3)
-
             return vrf, server
 
 
