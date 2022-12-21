306121e85d12852359fa213a2255d20a89a23e65
bentole <bentole@msn.com>
2022-12-21 21:12:52 +0000

    removed empty lines added earlier

--- plugins/modules/ios_vrf.py
+++ plugins/modules/ios_vrf.py
@@ -659,7 +659,6 @@ def check_declarative_intent_params(want, module, result):
         if result["changed"]:
             time.sleep(module.params["delay"])
         name = module.params["name"]
-
         rc, out, err = exec_command(module, "show vrf | include {0}".format(name))
         if rc == 0:
             data = out.strip().split()
@@ -669,7 +668,6 @@ def check_declarative_intent_params(want, module, result):
             interface = data[-1]
             for w in want:
                 if w["name"] == vrf:
-
                     if w.get("associated_interfaces") is None:
                         continue
                     for i in w["associated_interfaces"]:
@@ -707,7 +705,6 @@ def main():
         mutually_exclusive=mutually_exclusive,
         supports_check_mode=True,
     )
-
     result = {"changed": False}
     warnings = list()
     result["warnings"] = warnings
