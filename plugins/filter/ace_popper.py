a42b1f2712d423b5d01a3796bef794cce73c15a9
KB-perByte <sagpaul@redhat.com>
2023-02-16 16:05:35 +0530

    fix doc

--- plugins/filter/ace_popper.py
+++ plugins/filter/ace_popper.py
@@ -82,139 +82,139 @@ DOCUMENTATION = """
 
 EXAMPLES = r"""
 ##Playbook with filter plugin example
-  vars:
-    filter_options:
-      match_all: true
-    match_criteria:
-      afi: "ipv4"
-      source_address: "192.0.2.0"
-      destination_address: "192.0.3.0"
-    acls_data:
-      - acls:
-          - aces:
-              - destination:
-                  address: 192.0.3.0
-                  wildcard_bits: 0.0.0.255
-                dscp: ef
-                grant: deny
-                protocol: icmp
-                protocol_options:
-                  icmp:
-                    traceroute: true
-                sequence: 10
-                source:
-                  address: 192.0.2.0
-                  wildcard_bits: 0.0.0.255
-                ttl:
-                  eq: 10
-              - destination:
-                  host: 198.51.110.0
-                  port_protocol:
-                    eq: telnet
-                grant: deny
-                protocol: tcp
-                protocol_options:
-                  tcp:
-                    ack: true
-                sequence: 20
-                source:
-                  host: 198.51.100.0
-            acl_type: extended
-            name: "110"
-          - aces:
-              - destination:
-                  address: 198.51.101.0
-                  port_protocol:
-                    eq: telnet
-                  wildcard_bits: 0.0.0.255
-                grant: deny
-                protocol: tcp
-                protocol_options:
-                  tcp:
-                    ack: true
-                sequence: 10
-                source:
-                  address: 198.51.100.0
-                  wildcard_bits: 0.0.0.255
-                tos:
-                  service_value: 12
-              - destination:
-                  address: 192.0.4.0
-                  port_protocol:
-                    eq: www
-                  wildcard_bits: 0.0.0.255
-                dscp: ef
-                grant: deny
-                protocol: tcp
-                protocol_options:
-                  tcp:
-                    ack: true
-                sequence: 20
-                source:
-                  address: 192.0.3.0
-                  wildcard_bits: 0.0.0.255
-                ttl:
-                  lt: 20
-            acl_type: extended
-            name: "123"
-          - aces:
-              - grant: deny
-                sequence: 10
-                source:
-                  host: 192.168.1.200
-              - grant: deny
-                sequence: 20
-                source:
-                  address: 192.168.2.0
-                  wildcard_bits: 0.0.0.255
-            acl_type: standard
-            name: std_acl
-          - aces:
-              - destination:
-                  address: 192.0.3.0
-                  port_protocol:
-                    eq: www
-                  wildcard_bits: 0.0.0.255
-                grant: deny
-                option:
+vars:
+  filter_options:
+    match_all: true
+  match_criteria:
+    afi: "ipv4"
+    source_address: "192.0.2.0"
+    destination_address: "192.0.3.0"
+  acls_data:
+    - acls:
+        - aces:
+            - destination:
+                address: 192.0.3.0
+                wildcard_bits: 0.0.0.255
+              dscp: ef
+              grant: deny
+              protocol: icmp
+              protocol_options:
+                icmp:
                   traceroute: true
-                protocol: tcp
-                protocol_options:
-                  tcp:
-                    fin: true
-                sequence: 10
-                source:
-                  address: 192.0.2.0
-                  wildcard_bits: 0.0.0.255
-                ttl:
-                  eq: 10
-            acl_type: extended
-            name: test
-        afi: ipv4
-      - acls:
-          - aces:
-              - destination:
-                  any: true
-                  port_protocol:
-                    eq: telnet
-                dscp: af11
-                grant: deny
-                protocol: tcp
-                protocol_options:
-                  tcp:
-                    ack: true
-                sequence: 10
-                source:
-                  any: true
-                  port_protocol:
-                    eq: www
-            name: R1_TRAFFIC
-        afi: ipv6
+              sequence: 10
+              source:
+                address: 192.0.2.0
+                wildcard_bits: 0.0.0.255
+              ttl:
+                eq: 10
+            - destination:
+                host: 198.51.110.0
+                port_protocol:
+                  eq: telnet
+              grant: deny
+              protocol: tcp
+              protocol_options:
+                tcp:
+                  ack: true
+              sequence: 20
+              source:
+                host: 198.51.100.0
+          acl_type: extended
+          name: "110"
+        - aces:
+            - destination:
+                address: 198.51.101.0
+                port_protocol:
+                  eq: telnet
+                wildcard_bits: 0.0.0.255
+              grant: deny
+              protocol: tcp
+              protocol_options:
+                tcp:
+                  ack: true
+              sequence: 10
+              source:
+                address: 198.51.100.0
+                wildcard_bits: 0.0.0.255
+              tos:
+                service_value: 12
+            - destination:
+                address: 192.0.4.0
+                port_protocol:
+                  eq: www
+                wildcard_bits: 0.0.0.255
+              dscp: ef
+              grant: deny
+              protocol: tcp
+              protocol_options:
+                tcp:
+                  ack: true
+              sequence: 20
+              source:
+                address: 192.0.3.0
+                wildcard_bits: 0.0.0.255
+              ttl:
+                lt: 20
+          acl_type: extended
+          name: "123"
+        - aces:
+            - grant: deny
+              sequence: 10
+              source:
+                host: 192.168.1.200
+            - grant: deny
+              sequence: 20
+              source:
+                address: 192.168.2.0
+                wildcard_bits: 0.0.0.255
+          acl_type: standard
+          name: std_acl
+        - aces:
+            - destination:
+                address: 192.0.3.0
+                port_protocol:
+                  eq: www
+                wildcard_bits: 0.0.0.255
+              grant: deny
+              option:
+                traceroute: true
+              protocol: tcp
+              protocol_options:
+                tcp:
+                  fin: true
+              sequence: 10
+              source:
+                address: 192.0.2.0
+                wildcard_bits: 0.0.0.255
+              ttl:
+                eq: 10
+          acl_type: extended
+          name: test
+      afi: ipv4
+    - acls:
+        - aces:
+            - destination:
+                any: true
+                port_protocol:
+                  eq: telnet
+              dscp: af11
+              grant: deny
+              protocol: tcp
+              protocol_options:
+                tcp:
+                  ack: true
+              sequence: 10
+              source:
+                any: true
+                port_protocol:
+                  eq: www
+          name: R1_TRAFFIC
+      afi: ipv6
 
-  tasks:
-    - name: Remove ace entries from a provided data
-      ansible.builtin.debug:
-        msg: "{{ acls_data | cisco.ios.ace_popper(filter_options=filter_options, match_criteria=match_criteria) }}"
+tasks:
+  - name: Remove ace entries from a provided data
+    ansible.builtin.debug:
+      msg: "{{ acls_data | cisco.ios.ace_popper(filter_options=filter_options, match_criteria=match_criteria) }}"
 
 ##Output
 # PLAY [Filter plugin example ace_popper] ******************************************************************************************************************
