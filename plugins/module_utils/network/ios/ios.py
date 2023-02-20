b251e19e92e3464804db6cf88ac08c19a24f8495
KB-perByte <sagpaul@redhat.com>
2023-02-20 11:41:13 +0530

    fix sanity import issues

--- plugins/module_utils/network/ios/ios.py
+++ plugins/module_utils/network/ios/ios.py
@@ -32,7 +32,6 @@ __metaclass__ = type
 import json
 
 from ansible.module_utils._text import to_text
-from ansible.module_utils.basic import env_fallback
 from ansible.module_utils.connection import Connection, ConnectionError
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import to_list
 
