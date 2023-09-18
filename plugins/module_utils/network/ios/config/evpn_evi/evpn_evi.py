382b8d5dc4a868eae41af8b1162d9af9db957f36
PadminiSivaraj <108111540+PadminiSivaraj@users.noreply.github.com>
2023-09-18 17:13:37 +0530

    Update evpn_evi.py - removed unused import

--- plugins/module_utils/network/ios/config/evpn_evi/evpn_evi.py
+++ plugins/module_utils/network/ios/config/evpn_evi/evpn_evi.py
@@ -18,8 +18,6 @@ necessary to bring the current configuration to its desired end-state is
 created.
 """
 
-from copy import deepcopy
-
 from ansible.module_utils.six import iteritems
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module import (
     ResourceModule,
