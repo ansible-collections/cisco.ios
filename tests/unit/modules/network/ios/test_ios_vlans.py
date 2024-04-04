487238c14f7bcb9370d88b3591e1d397e8a3b14a
Vinay Mulugund <roverflow5@gmail.com>
2024-04-04 18:07:47 +0530

    fix precommit

--- tests/unit/modules/network/ios/test_ios_vlans.py
+++ tests/unit/modules/network/ios/test_ios_vlans.py
@@ -1240,7 +1240,7 @@ class TestIosVlansModule(TestIosModule):
         ]
         self.assertEqual(result["commands"], commands)
 
-    def test_ios_delete_vlans_config(self):
+    def test_ios_delete_vlans_config_2(self):
         self.mock_l2_device_command.side_effect = True
         self.mock_execute_show_command_conf.side_effect = ""
         self.execute_show_command.return_value = dedent(
