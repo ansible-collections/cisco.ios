1926833f4dfcd7a3f01e7d681f3c782a61790cb7
Girish <kaih5tri@gmail.com>
2024-08-30 14:53:27 +0530

    fixed sanity errors

--- plugins/module_utils/network/ios/config/snmp_server/snmp_server.py
+++ plugins/module_utils/network/ios/config/snmp_server/snmp_server.py
@@ -20,7 +20,8 @@ created.
 
 from copy import deepcopy
 
-from ansible.module_utils.six import iteritems
+
+# from ansible.module_utils.six import iteritems
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module import (
     ResourceModule,
 )
