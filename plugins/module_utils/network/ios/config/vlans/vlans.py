cb8db036e3d26614f078dead92f69d40a8463051
jornivarholland <jornivarholland@icloud.com>
2025-10-07 17:27:36 +0200

    fix: add comment

--- plugins/module_utils/network/ios/config/vlans/vlans.py
+++ plugins/module_utils/network/ios/config/vlans/vlans.py
@@ -117,6 +117,7 @@ class Vlans(ResourceModule):
             excluded_vlans = {k: v for k, v in haved.items() if k not in wantd or not wantd}
             haved = {k: v for k, v in haved.items() if k in wantd or not wantd}
             for k, have in excluded_vlans.items():
+                # Skip reserved VLANs 1002–1005
                 if int(k) in [1002, 1003, 1004, 1005]:
                     continue
                 self.purge(have, resource)
