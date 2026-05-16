ca1cd06d1bf8785d4f47fe4a2f97ad8ac2826263
Alexander Haase <haase@fh-aachen.de>
2026-05-16 14:40:16 +0200

    Fix ios_user idempocy test case

--- tests/unit/modules/network/ios/test_ios_user.py
+++ tests/unit/modules/network/ios/test_ios_user.py
@@ -186,7 +186,7 @@ class TestIosUserModule(TestIosModule):
             "ip ssh pubkey-chain",
             "username purger",
             "no key-hash ssh-rsa A019918340A1E9183388D9A675603036",
-            "no key-hash ssh-rsa B08C01820F6E95E284B3388FDFF5395A",
+            "no key-hash ssh-rsa A0806D49409C0B793776C76A870D9928",
             "exit",
             "exit",
             "ip ssh pubkey-chain",
@@ -212,7 +212,7 @@ class TestIosUserModule(TestIosModule):
         self.assertEqual(result["commands"], commands)
 
     def test_ios_user_add_sshkey_purge_keys_true_idempotent(self):
-        set_module_args(dict(name="purger", sshkey="eHWacB2==", purge_keys=True))
+        set_module_args(dict(name="purger", sshkey=["eHWacB2==", "dHJhaWxpbmcK"], purge_keys=True))
         commands = []
         result = self.execute_module(changed=False, commands=commands)
         self.assertEqual(result["commands"], commands)
